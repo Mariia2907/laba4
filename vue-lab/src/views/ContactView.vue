f0393d66204ac4dc2e01648f1fe4e9746bf29d81
Maria Honta <201110089+Mariia2907@users.noreply.github.com>
2025-11-25 08:28:58 +0200

    laba 4 commit

---
+++ vue-lab/src/views/ContactView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import UserCard from '@/components/UserCard.vue';
+</script>
+
+<template>
+  <div class="contact-page">
+    <h1>Сторінка контактів</h1>
+    <p>Це наша нова сторінка контактів!</p>
+    <UserCard username="Student_1" :isActive="5432" />
+    <UserCard username="Student_2" :isActive="false" />
+  </div>
+</template>
+/*Type 'number' is not assignable to type 'boolean'.(Помилка виникає через те,що ми передаємо часло замість буліана)*/
+
+<style scoped>
+/* Стилі <style scoped> застосовуються лише до цього компонента */
+.contact-page {
+  padding: 20px;
+}
+</style>
